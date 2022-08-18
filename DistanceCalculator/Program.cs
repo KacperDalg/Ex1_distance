ecc229a7fafe65dd345a6255048b9e5e7ade34b4
Kacper <kacperdalgiewicz@gmail.com>
2022-08-18 14:09:53 +0200

    initial commit

---
+++ DistanceCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj dystans w kilometrach: ");
+            double distKilometers = Convert.ToDouble(Console.ReadLine());
+            double distMeters = distKilometers * 1000;
+            Console.WriteLine($"{distKilometers} kilometrów to {distMeters} metrów.");
+
+            var path = "plik.txt";
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine(distMeters);
+            sw.Close();
+            Console.WriteLine("---");
+            Console.WriteLine("Zapisano do pliku");
+
+        }
+    }
+
+}
