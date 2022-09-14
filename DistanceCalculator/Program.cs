ca5d34b5428f1d56b381bd17f0cafad753693bd9
Kacper <kacperdalgiewicz@gmail.com>
2022-09-14 19:48:36 +0200

    new file

--- DistanceCalculator/Program.cs
+++ DistanceCalculator/Program.cs
@@ -1,26 +1,17 @@
-﻿using System.IO;
-
-namespace ConsoleApp1
-
+﻿namespace ConsoleApp1;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Podaj dystans w kilometrach: ");
-            double distKilometers = Convert.ToDouble(Console.ReadLine());
-            double distMeters = distKilometers * 1000;
-            Console.WriteLine($"{distKilometers} kilometrów to {distMeters} metrów.");
-
-            var path = "plik.txt";
-            StreamWriter sw = new StreamWriter(path, true);
-            sw.WriteLine(distMeters);
-            sw.Close();
-            Console.WriteLine("---");
-            Console.WriteLine("Zapisano do pliku");
-
-        }
+        Console.WriteLine("Podaj dystans w kilometrach: ");
+        double distanceInKilometers = Convert.ToDouble(Console.ReadLine());
+        double distanceInMeters = distanceInKilometers * 1000;
+        Console.WriteLine($"{distanceInKilometers} kilometrów to {distanceInMeters} metrów.");
+        var path = "plik.txt";
+        StreamWriter sw = new StreamWriter(path, true);
+        sw.WriteLine(distanceInMeters);
+        sw.Close();
+        Console.WriteLine("---");
+        Console.WriteLine("Zapisano do pliku");
     }
-
 }
